589dc8da79d0ca160285c88790b086d047089c78
Nikka Canaria <nikkacanaria@mabinicolleges.edu.ph>
2026-05-21 22:48:02 +0800

    Update login.js

--- record/record/static/js/login.js
+++ record/record/static/js/login.js
@@ -1,1 +1,18 @@
+document.querySelector("form").addEventListener("submit", function(e) {
+    e.preventDefault();
 
+    let formData = new FormData(this);
+
+    fetch("", {
+        method: "POST",
+        body: formData
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.status === "success") {
+            window.location.href = data.redirect;
+        } else {
+            alert(data.message);
+        }
+    });
+});
