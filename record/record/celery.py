1c858c7ed00398aacd60b395f40e67254ecdb448
Nikka Canaria <nikkacanaria@mabinicolleges.edu.ph>
2026-05-21 23:17:43 +0800

    Update celery.py

--- record/record/celery.py
+++ record/record/celery.py
@@ -1,1 +1,10 @@
+import os
+from celery import Celery
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'record.settings')
+
+app = Celery('record')
+
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+app.autodiscover_tasks()
